faa21ef8c10f46f984bfab7aa88382a8550dcafd
SUGGESTIED ✨ <74950245+suggestied@users.noreply.github.com>
2025-10-06 18:39:01 +0200

    Remove modules

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -23,7 +23,14 @@ export default function Layout(
       <AppSidebar variant="inset" />
       <SidebarInset>
         <SiteHeader />
+        <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+      <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
         {children}
+
+      </div>
+      </div>
+      </div>
       </SidebarInset>
       {/* <SecondSidebar side="right" variant="inset" collapsible="icon" /> */}
     </SidebarProvider>
--- src/modules/work-orders/components/WorkOrderListServer.tsx
+++
@@ -1,9 +0,0 @@
-import { workOrdersApi } from '../data'
-import { WorkOrderListClient } from '@/modules/work-orders/components/WorkOrderListClient'
-
-export async function WorkOrderListServer() {
-  // Server-side data fetching - no loading state needed!
-  const workOrders = await workOrdersApi.getAll()
-
-  return <WorkOrderListClient initialWorkOrders={workOrders} />
-}
